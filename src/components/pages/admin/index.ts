116179acc8885fa5d7064019826dce18729858cc
hafizfaisalali <hafizfaisalali605@gmail.com>
2026-02-02 19:16:29 +0500

    fix: vercel build error

--- src/components/pages/admin/index.ts
+++ src/components/pages/admin/index.ts
@@ -1,25 +1,25 @@
-import UsersPage from './UsersPage';
-import OrdersPage from './OrdersPage';
 import BookingPage from './BookingPage';
-import RevenuePage from './RevenuePage';
-import ReportsPage from './ReportsPage';
-import ProfilePage from './ProfilePage';
-import WorkersPage from './WorkersPage';
 import DashboardPage from './DashboardPage';
-import ProductsPage from './ProductsPage';
-import ServicesPage from './ServicesPage';
 import NewServicePage from './NewServicePage';
+import OrdersPage from './OrdersPage';
+import ProductsPage from './ProductsPage';
+import ProfilePage from './ProfilePage';
+import ReportsPage from './ReportsPage';
+import RevenuePage from './RevenuePage';
+import ServicesPage from './ServicesPage';
+import UsersPage from './UsersPage';
+import WorkersPage from './WorkersPage';
 
 export {
-  UsersPage,
-  OrdersPage,
-  WorkersPage,
   BookingPage,
-  RevenuePage,
-  ReportsPage,
-  ProfilePage,
-  ProductsPage,
-  ServicesPage,
   DashboardPage,
   NewServicePage,
+  OrdersPage,
+  ProductsPage,
+  ProfilePage,
+  ReportsPage,
+  RevenuePage,
+  ServicesPage,
+  UsersPage,
+  WorkersPage,
 };
