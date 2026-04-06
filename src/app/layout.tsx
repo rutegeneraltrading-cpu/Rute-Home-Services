e9c00684310ffaa167cdadf9cb21051e205b8db6
hafizfaisalali <hafizfaisalali605@gmail.com>
2026-04-06 22:12:25 +0500

    fix: google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import { QueryProvider } from '@/lib/client/providers';
 import { Toaster } from '@/components/ui';
 import { ROOT_METADATA, STRUCTURED_DATA } from '@/lib/seo';
@@ -6,7 +7,7 @@ import 'react-phone-input-2/lib/style.css';
 import './globals.css';
 
 export const metadata: Metadata = ROOT_METADATA;
-
+const measurementId = process.env.NEXT_PUBLIC_GA_ID || 'G-XYZ';
 export default function RootLayout({
   children,
 }: {
@@ -24,6 +25,7 @@ export default function RootLayout({
         <QueryProvider>{children}</QueryProvider>
         <Toaster />
       </body>
+      <GoogleAnalytics gaId={measurementId} />
     </html>
   );
 }
